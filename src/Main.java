4e2c401fb60eba34821d78b7f4e87488e2e928af
nisha-dev2 <nipunisha274@gmail.com>
2024-05-27 16:08:19 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
